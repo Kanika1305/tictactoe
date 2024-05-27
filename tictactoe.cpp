5439b4442c2cfe3abe736a00b5e1c1513e95ce80
Kanika1305 <148940806+Kanika1305@users.noreply.github.com>
2024-05-27 12:55:16 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -7,8 +7,7 @@ void display_grid(char grid[][3],int row,int col){
         {
             cout<<grid[i][j];
             if(j==2) break;
-            cout<<"   |";
-             
+            cout<<"   |";           
         }
         cout<<endl;
          if(i==2) break;
@@ -19,9 +18,11 @@ void display_grid(char grid[][3],int row,int col){
     int pos1;
     cout << "ENTER POSITION FOR PLAYER 1" << endl;
     cin >> pos1;
+   
     if (pos1 == 1) {
+         
         if (grid[0][0] == 'X' || grid[0][0] == 'O') cout << "PLACE OCCUPIED";
-        else  grid[0][0] = 'X';
+        else {  grid[0][0] = 'X';}
         }
      else if (pos1 == 2) {
         if (grid[0][1] == 'X' || grid[0][1] == 'O')  cout << "PLACE OCCUPIED";
@@ -127,9 +128,8 @@ int winner(char (*grid)[3],int row ,int col){
     if(grid[0][2]==grid[1][1]&& grid[1][1]==grid[2][0]){
         if(grid[2][0]=='X') return f=1 ; else return f=2 ;
     }
-    
+    return 0;
 }
-
 int main(){
     int i ;
     char grid[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
@@ -139,29 +139,33 @@ int main(){
     {
         if(i%2==0){
             int w ;
+            cout << "\033[1;35m";
             cout<<"PLAYER 1 TURN:"<<endl;
+          
             insert_p1(grid, 3, 3);
+             
              display_grid(grid,3,3);
             w= winner(grid,3,3);
+            cout << "\033[1;32m" ;
             if(w==1){ cout<<"PLAYER 1 WINS"<<endl;
              display_grid(grid,3,3);
              break ;}
         }
         else {
             int w;
+            cout << "\033[1;34m";
             cout<<"PLAYER 2 TURN:"<<endl;
             insert_p2(grid, 3, 3);
              display_grid(grid,3,3);
             w= winner(grid,3,3);
+            cout << "\033[1;32m" ;
             if(w==2){ cout<<"PLAYER 2 WINS"<<endl;
              display_grid(grid,3,3);
              break ;
         }
-             
-
         }
     }
-     if(i==9) cout<<"DRAW" ;
+     cout << "\033[0m" ;if(i==9) cout<<" The Game is DRAW" ;
 
 return 0;
 }
